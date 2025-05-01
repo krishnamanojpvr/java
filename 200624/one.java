08e3151065004290f10d6cb99de12eac9f4a2c82
krishnamanojpvr <pvrkmsbunny@gmail.com>
2025-05-01 08:06:02 +0530

    java

--- 200624/one.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-public class one{
-    public static void main(String args[]){
-        List<Integer> l = new Stack<>();
-        l.push(20);
-    }
-}
--- libman/Book.java
+++
@@ -1,9 +0,0 @@
-public class Book {
-    String name; int ISBN; boolean isIssued;
-    public Book(String name, int iSBN, boolean isIssued) {
-      super();
-      this.name = name;
-      ISBN = iSBN;
-      this.isIssued= isIssued;
-    }
-  }
